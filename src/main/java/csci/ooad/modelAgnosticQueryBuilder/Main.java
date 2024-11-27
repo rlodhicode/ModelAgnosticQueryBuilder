cce082b93114bb97d2fb704f4e9c1b6dbe77b2db
rlodhicode <rayaan.lodhi@gmail.com>
2024-11-27 13:53:24 -0700

    simplified query, added TODOs/NOTEs

--- src/main/java/csci/ooad/modelAgnosticQueryBuilder/Main.java
+++ src/main/java/csci/ooad/modelAgnosticQueryBuilder/Main.java
@@ -3,31 +3,29 @@ package csci.ooad.modelAgnosticQueryBuilder;
 import csci.ooad.modelAgnosticQueryBuilder.Query.Query;
 import csci.ooad.modelAgnosticQueryBuilder.Query.QueryBuilder;
 import csci.ooad.modelAgnosticQueryBuilder.QueryExecutor.IQueryExecutor;
+import csci.ooad.modelAgnosticQueryBuilder.QueryExecutor.MongoQueryExecutor;
 import csci.ooad.modelAgnosticQueryBuilder.QueryExecutor.PostgreSQLQueryExecutor;
 
 // TODO: implement tests for query creation
 public class Main {
     public static void main(String[] args) {
-        // TODO: replace this query with queryTemp when local mongoDB collection is renamed to sharded_demo
         Query query = QueryBuilder.newBuilder()
-                .useDatabase("csci5448")
-                .from("sharded_trips")
-                .where(new Condition("tripduration", Condition.GREATER_THAN_OR_EQUAL, 100000))
-                .where(new Condition("usertype", Condition.EQUALS, "Customer"))
-                .build();
-
-//        IQueryExecutor mongoExecutor = new MongoQueryExecutor();
-//        mongoExecutor.execute(query);
-
-        Query queryTemp = QueryBuilder.newBuilder()
                 .useDatabase("csci5448")
                 .from("sharded_demo")
                 .where(new Condition("tripduration", Condition.GREATER_THAN_OR_EQUAL, 100000))
                 .where(new Condition("usertype", Condition.EQUALS, "Customer"))
                 .build();
 
+        // NOTE: sharded mongo setup is a bit complicated and we don't have a DOCKERFILE to
+        //          automate that process due to the requirements of Mongo DB Tools, and
+        //          set configuration requirements. To setup the mongo repository that is used
+        //          in this project, clone and follow the setup steps of the mongo_sharded_project
+        //          repository: https://github.com/rlodhicode/mongo-sharded-project
+        IQueryExecutor mongoExecutor = new MongoQueryExecutor();
+        mongoExecutor.execute(query);
+
         IQueryExecutor postgresExecutor = new PostgreSQLQueryExecutor();
-        postgresExecutor.execute(queryTemp);
+        postgresExecutor.execute(query);
     }
 }
 
--- src/main/java/csci/ooad/modelAgnosticQueryBuilder/QueryExecutor/IQueryExecutor.java
+++ src/main/java/csci/ooad/modelAgnosticQueryBuilder/QueryExecutor/IQueryExecutor.java
@@ -2,6 +2,7 @@ package csci.ooad.modelAgnosticQueryBuilder.QueryExecutor;
 
 import csci.ooad.modelAgnosticQueryBuilder.Query.Query;
 
+// TODO: reconcile. Command pattern over strategy?
 public interface IQueryExecutor {
     void execute(Query query);
 }
